93c7ea6e226166b2add9478a6455c30a0c468768
Zartab Nakhwa <zartab@codewithz.com>
2024-10-28 14:17:15 +0530

    Parameter Navigation - 1

---
+++ Components/Pages/EmployeeDetail.razor.cs
@@ -0,0 +1,21 @@
+﻿using BethanysPieHRMSApp.Services;
+using BethanysPieHRMSApp.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieHRMSApp.Components.Pages
+{
+    public partial class EmployeeDetail
+    {
+        [Parameter]
+        public int EmployeeId { get; set; }
+
+        public Employee Employee { get; set; }
+
+
+        protected override void OnInitialized()
+        {
+            Employee = MockDataService.Employees.Single(e => e.EmployeeId == EmployeeId);
+        }
+
+    }
+}
