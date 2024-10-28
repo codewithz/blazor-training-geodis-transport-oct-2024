e4259400a166bf05d96dcca92dd530be8723924d
Zartab Nakhwa <zartab@codewithz.com>
2024-10-28 12:59:15 +0530

    Added Employee Overview Component

--- Components/Pages/EmployeeOverview.razor.cs
+++ Components/Pages/EmployeeOverview.razor.cs
@@ -1,6 +1,16 @@
-﻿namespace BethanysPieHRMSApp.Components.Pages
+﻿using BethanysPieHRMSApp.Services;
+using BethanysPieHRMSApp.Shared.Domain;
+
+namespace BethanysPieHRMSApp.Components.Pages
 {
     public partial class EmployeeOverview
     {
+
+        public List<Employee> Employees { get; set; }=default!;
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employees =  MockDataService.Employees;
+        }
     }
 }
