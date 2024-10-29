6bca3afffb2e31ec7352783d491b3f40d6c7b3c7
Zartab Nakhwa <zartab@codewithz.com>
2024-10-29 14:23:35 +0530

    EF Core : Part 3: Connection String and Configuration Addition to Program.cs

--- Program.cs
+++ Program.cs
@@ -1,10 +1,17 @@
 using BethanysPieHRMSApp.Components;
+using BethanysPieHRMSApp.Data;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddRazorComponents()
     .AddInteractiveServerComponents();
+
+builder.Services.AddDbContextFactory<AppDbContext>(options =>
+{
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
 
 var app = builder.Build();
 
