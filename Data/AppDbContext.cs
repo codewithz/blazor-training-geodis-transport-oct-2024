61d6a49eb44c512ae4fb39f94962bca24f4c5a09
Zartab Nakhwa <zartab@codewithz.com>
2024-10-29 14:16:54 +0530

    EF Core : Part 2: Created AppDBContext File

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,19 @@
+﻿using BethanysPieHRMSApp.Shared.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace BethanysPieHRMSApp.Data
+{
+    public class AppDbContext:DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Employee> Employees { get; set; } 
+
+        public DbSet<JobCategory> JobCategories { get; set; } 
+
+        public DbSet<Country> Countries { get; set; } 
+    }
+}
