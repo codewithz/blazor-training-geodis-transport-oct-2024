834ab88ebfb7c48f24c6f0da7613be9a81566e5d
Zartab Nakhwa <zartab@codewithz.com>
2024-10-30 12:12:03 +0530

    Forms Basics

---
+++ Components/Forms/DummyForm.razor.cs
@@ -0,0 +1,25 @@
+﻿using BethanysPieHRMSApp.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieHRMSApp.Components.Forms
+{
+    public partial class DummyForm:ComponentBase
+    {
+        public Employee Employee { get; set; } 
+
+        protected override async Task OnInitializedAsync()
+        {
+            Employee = new Employee
+            {
+                FirstName = "John",
+                LastName = "Doe",
+            };
+          
+        }
+
+        public void Save()
+        {
+            Employee.FirstName = "Zartab";
+        }
+    }
+}
