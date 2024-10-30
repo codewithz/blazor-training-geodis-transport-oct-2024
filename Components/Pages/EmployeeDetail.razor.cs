16b2bd7104a30606f8efb858ee0b6eb7dd0029c1
Zartab Nakhwa <zartab@codewithz.com>
2024-10-30 09:59:10 +0530

    Loading Data in Application from DB

--- Components/Pages/EmployeeDetail.razor.cs
+++ Components/Pages/EmployeeDetail.razor.cs
@@ -1,20 +1,24 @@
-﻿using BethanysPieHRMSApp.Services;
+﻿using BethanysPieHRMSApp.Contracts.Services;
+using BethanysPieHRMSApp.Services;
 using BethanysPieHRMSApp.Shared.Domain;
 using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieHRMSApp.Components.Pages
 {
-    public partial class EmployeeDetail
+    public partial class EmployeeDetail:ComponentBase
     {
         [Parameter]
         public int EmployeeId { get; set; }
 
-        public Employee Employee { get; set; }
+        public Employee Employee { get; set; } =new Employee();
 
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
 
-        protected override void OnInitialized()
+        protected async override Task OnInitializedAsync()
         {
-            Employee = MockDataService.Employees.Single(e => e.EmployeeId == EmployeeId);
+            //Employee = MockDataService.Employees.Single(e => e.EmployeeId == EmployeeId);
+            Employee = await EmployeeService.GetEmployeeById(EmployeeId);
         }
 
         private void ChangeHolidayState()
--- Components/Pages/EmployeeOverview.razor.cs
+++ Components/Pages/EmployeeOverview.razor.cs
@@ -1,21 +1,25 @@
-﻿using BethanysPieHRMSApp.Services;
+﻿using BethanysPieHRMSApp.Contracts.Services;
+using BethanysPieHRMSApp.Services;
 using BethanysPieHRMSApp.Shared.Domain;
 using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieHRMSApp.Components.Pages
 {
-    public partial class EmployeeOverview:ComponentBase
+    public partial class EmployeeOverview : ComponentBase
     {
 
         private Employee? _selectedEmployee;
         public List<Employee> Employees { get; set; } = new List<Employee>();
 
-        private string Title= "Employee Overview";
+        private string Title = "Employee Overview";
 
+        [Inject]
+        public IEmployeeService EmployeeService { get; set; }
         protected override async Task OnInitializedAsync()
         {
-            await Task.Delay(1000);
-            Employees =  MockDataService.Employees;
+            //await Task.Delay(1000);
+            //Employees =  MockDataService.Employees;
+            Employees = (await EmployeeService.GetAllEmployees()).ToList();
         }
 
         public void ShowQuickView(Employee employee)
