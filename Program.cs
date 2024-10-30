b2863c6e19ae5a3fcabc0421ed5a9f0e3a6a33c6
Zartab Nakhwa <zartab@codewithz.com>
2024-10-30 09:46:38 +0530

    Register our Service and Repository with Dependency Injection Container

--- Program.cs
+++ Program.cs
@@ -1,5 +1,9 @@
 using BethanysPieHRMSApp.Components;
+using BethanysPieHRMSApp.Contracts.Repositories;
+using BethanysPieHRMSApp.Contracts.Services;
 using BethanysPieHRMSApp.Data;
+using BethanysPieHRMSApp.Repositories;
+using BethanysPieHRMSApp.Services;
 using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -12,6 +16,9 @@ builder.Services.AddDbContextFactory<AppDbContext>(options =>
 {
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
+
+builder.Services.AddScoped<IEmployeeRepository, EmployeeRepository>();
+builder.Services.AddScoped<IEmployeeService, EmployeeService>();
 
 var app = builder.Build();
 
