a82f9e36cb823d71476a72e7e98c1c0ab4e99289
Zartab Nakhwa <zartab@codewithz.com>
2024-10-30 12:25:26 +0530

    Employee Add FOrm - first field

---
+++ Components/Pages/EmployeeAdd.razor.cs
@@ -0,0 +1,18 @@
+﻿using BethanysPieHRMSApp.Shared.Domain;
+using Microsoft.AspNetCore.Components;
+
+namespace BethanysPieHRMSApp.Components.Pages
+{
+    public partial class EmployeeAdd
+    {
+        [SupplyParameterFromForm]
+        public Employee Employee { get; set; }
+
+        protected override void OnInitialized()
+        {
+            Employee ??= new();
+        }
+
+
+    }
+}
