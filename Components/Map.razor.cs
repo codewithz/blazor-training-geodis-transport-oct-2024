c5823e40d072cb70ce0a56d3017a289707f6c2d1
Zartab Nakhwa <zartab@codewithz.com>
2024-10-30 15:08:24 +0530

    Integrated Map with Component using JSInterop

--- Components/Map.razor.cs
+++ Components/Map.razor.cs
@@ -1,6 +1,29 @@
-﻿namespace BethanysPieHRMSApp.Components
+﻿using BethanysPieHRMSApp.Shared.Model;
+using Microsoft.AspNetCore.Components;
+using Microsoft.JSInterop;
+
+namespace BethanysPieHRMSApp.Components
 {
     public partial class Map
     {
+
+        string elementId=$"map-{Guid.NewGuid():D}";
+        [Parameter]
+        public double Zoom { get; set; }
+
+        [Parameter]
+        public List<Marker> Markers { get; set; } = new List<Marker>();
+
+        [Inject]
+        public IJSRuntime JSRuntime { get; set; }
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            await JSRuntime.InvokeVoidAsync(
+                "deliveryMap.showOrUpdate",
+                elementId,
+                Markers
+                );
+        }
     }
 }
--- Components/Pages/EmployeeDetail.razor.cs
+++ Components/Pages/EmployeeDetail.razor.cs
@@ -1,6 +1,7 @@
 ﻿using BethanysPieHRMSApp.Contracts.Services;
 using BethanysPieHRMSApp.Services;
 using BethanysPieHRMSApp.Shared.Domain;
+using BethanysPieHRMSApp.Shared.Model;
 using Microsoft.AspNetCore.Components;
 
 namespace BethanysPieHRMSApp.Components.Pages
@@ -15,10 +16,28 @@ namespace BethanysPieHRMSApp.Components.Pages
         [Inject]
         public IEmployeeService EmployeeService { get; set; }
 
+        public List<Marker> MapMarkers { get; set; } = new List<Marker>();
+
         protected async override Task OnInitializedAsync()
         {
             //Employee = MockDataService.Employees.Single(e => e.EmployeeId == EmployeeId);
             Employee = await EmployeeService.GetEmployeeById(EmployeeId);
+
+            if(Employee.Latitude.HasValue && Employee.Longitude.HasValue)
+            {
+                MapMarkers = new List<Marker>
+                {
+                    new Marker
+                    {
+                        Description=$"{Employee.FirstName} {Employee.LastName}",
+                        ShowPopup=false,
+                        X=(double)Employee.Longitude,
+                        Y=(double)Employee.Latitude
+
+                    }
+                };
+            }
+
         }
 
         private void ChangeHolidayState()
